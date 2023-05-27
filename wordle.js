74e3e5c5fd5eb80bca0b61b3142949c958ac3a26
Belov <ybelo@softserveinc.com>
2023-05-27 14:17:57 +0300

    wordle-ua

--- wordle.js
+++ wordle.js
@@ -1,4 +1,3 @@
-// Get DOM elements
 const submitBtn = document.getElementById('enter');
 const restartBtn = document.getElementById('restart');
 const letterButtons = document.querySelectorAll('.letter');
@@ -6,107 +5,23 @@ const attemptsTable = document.getElementById('attemptsTable');
 const deleteBtn = document.getElementById('delete');
 const feedback = document.getElementById('feedback');
 
+const maxAttempts = 6;
+let words;
 let targetWord;
 let attempts = 0;
 let columnIndex = 0;
-const maxAttempts = 6;
-let words;
-
-// Generate a random five-letter word
-function generateRandomWord() {
-    const request = new XMLHttpRequest();
-    request.open('GET', 'words.txt', false);
-    request.send();
-    const wordsString = request.responseText.replace(/[\n\r]/g, ''); // Remove new lines
-    words = wordsString.split(',');
-    const randomIndex = Math.floor(Math.random() * words.length);
-    return words[randomIndex];
-}
-
-function handleGuess() {
-    if (columnIndex !== 5) {
-        return;
-    }
-    let currentRow = document.getElementById('attemptsTable').rows.item(attempts).cells;
-    let guess = '';
-    for (let j = 0; j < currentRow.length; j++) {
-        guess += currentRow.item(j).innerHTML;
-    }
-    if (!words.includes(guess)) {
-        // Guess is not in the words list
-        showMessage("This word is not in the list", 1000);
-        return false;
-    }
-    let isGuessCorrect = checkGuess(guess, targetWord);
-    if (isGuessCorrect) {
-        showMessage("Congratulations! You guessed the word correctly.", 2000, "Press the 'New Game' button to restart.");
-        deleteBtn.disabled = true;
-        return true;
-    } else {
-        attempts++;
-        columnIndex = 0;
-    }
-}
-
-function checkGuess(guess, targetWord) {
-    let count = 0;
-    for (let i = 0; i < targetWord.length; i++) {
-        const targetChar = targetWord[i];
-        const guessChar = guess[i];
-        const button = document.getElementById(guessChar);
-
-        if (guessChar === targetChar) {
-            const cell = document.querySelector(`#cell-${attempts}-${i}`);
-            cell.style.backgroundColor = 'rgb(99,178,46)';
-            button.style.backgroundColor = 'rgb(99,178,46)';
-            ++count;
-        } else if (targetWord.includes(guessChar)) {
-            const cell = document.querySelector(`#cell-${attempts}-${i}`);
-            cell.style.backgroundColor = 'rgb(234,202,73)';
-            button.style.backgroundColor = 'rgb(234,202,73)';
-        } else {
-            button.style.backgroundColor = 'rgb(36, 36, 36)';
-        }
-    }
-
-    return count === 5;
-}
-
-function handleLetterPress(event) {
-    if (columnIndex == 5) {
-        return;
-    }
-    const letter = event.target.textContent;
-    const currentRow = attempts;
-
-    const cell = document.querySelector(`#cell-${currentRow}-${columnIndex}`);
-
-    cell.textContent = letter;
-
-    // Move to the next column
-    columnIndex++;
-
-}
-
-function handleDelete() {
-    if (columnIndex === 0) {
-        return;
-    }
-    columnIndex--;
-    const cell = document.querySelector(`#cell-${attempts}-${columnIndex}`);
-    if (cell.textContent === '') {
-        // No attempts made yet, nothing to delete
-        return;
-    }
-    // Clear the current cell
-    cell.textContent = '';
-}
-
-function handleRestart() {
-    initializeGame();
-}
 
 function initializeGame() {
+    function generateRandomWord() {
+        const request = new XMLHttpRequest();
+        request.open('GET', 'words.txt', false);
+        request.send();
+        const wordsString = request.responseText.replace(/[\n\r]/g, ''); // Remove new lines
+        words = wordsString.split(',');
+        const randomIndex = Math.floor(Math.random() * words.length);
+        return words[randomIndex];
+    }
+
     targetWord = generateRandomWord();
     attempts = 0;
     columnIndex = 0;
@@ -142,8 +57,6 @@ function showMessage(message, duration, appendMessage) {
     popUp.className = "popup-message";
     popUp.textContent = message;
     document.body.appendChild(popUp);
-
-    // Center the pop-up message
     popUp.style.position = "fixed";
     popUp.style.top = "35%";
     popUp.style.left = "50%";
@@ -163,6 +76,103 @@ function showMessage(message, duration, appendMessage) {
         document.body.removeChild(popUp);
     }, duration);
 }
+
+function handleGuess() {
+    if (attempts === maxAttempts) {
+        showMessage(
+            "Досягнуто максимальної кількості спроб.",
+            2000,
+            "Щоб перезапустити гру, натисніть кнопку «Нова гра»."
+        );
+        deleteBtn.disabled = true;
+        return true;
+    }
+    if (columnIndex !== 5) {
+        return;
+    }
+    let currentRow = document.getElementById('attemptsTable').rows.item(attempts).cells;
+    let guess = '';
+    for (let j = 0; j < currentRow.length; j++) {
+        guess += currentRow.item(j).innerHTML;
+    }
+    if (!words.includes(guess)) {
+        // Guess is not in the words list
+        showMessage("Цього слова немає в списку.", 1000);
+        return false;
+    }
+    let isGuessCorrect = checkGuess(guess, targetWord);
+    if (isGuessCorrect) {
+        showMessage("Вітаю! Ви вгадали слово.",
+            2000,
+            "Щоб перезапустити гру, натисніть кнопку «Нова гра».");
+        deleteBtn.disabled = true;
+        return true;
+    } else {
+        attempts++;
+        columnIndex = 0;
+    }
+    if (attempts === maxAttempts) {
+        showMessage(
+            "Досягнуто максимальної кількості спроб.",
+            2000,
+            "Щоб перезапустити гру, натисніть кнопку «Нова гра»."
+        );
+        deleteBtn.disabled = true;
+    }
+}
+
+function checkGuess(guess, targetWord) {
+    let count = 0;
+    for (let i = 0; i < targetWord.length; i++) {
+        const targetChar = targetWord[i];
+        const guessChar = guess[i];
+        const button = document.getElementById(guessChar);
+
+        if (guessChar === targetChar) {
+            const cell = document.querySelector(`#cell-${attempts}-${i}`);
+            cell.style.backgroundColor = 'rgb(99,178,46)';
+            button.style.backgroundColor = 'rgb(99,178,46)';
+            ++count;
+        } else if (targetWord.includes(guessChar)) {
+            const cell = document.querySelector(`#cell-${attempts}-${i}`);
+            cell.style.backgroundColor = 'rgb(234,202,73)';
+            button.style.backgroundColor = 'rgb(234,202,73)';
+        } else {
+            button.style.backgroundColor = 'rgb(36, 36, 36)';
+        }
+    }
+    return count === 5;
+}
+
+function handleLetterPress(event) {
+    if (columnIndex == 5) {
+        return;
+    }
+    const letter = event.target.textContent;
+    const cell = document.querySelector(`#cell-${attempts}-${columnIndex}`);
+    cell.textContent = letter;
+    // Move to the next column
+    columnIndex++;
+}
+
+function handleDelete() {
+    if (columnIndex === 0) {
+        return;
+    }
+    columnIndex--;
+    const cell = document.querySelector(`#cell-${attempts}-${columnIndex}`);
+    if (cell.textContent === '') {
+        // No attempts made yet, nothing to delete
+        return;
+    }
+    // Clear the current cell
+    cell.textContent = '';
+}
+
+function handleRestart() {
+    initializeGame();
+}
+
 const rulesButton = document.getElementById("rules-button");
 const popUp = document.createElement("div");
 popUp.className = "popup";
